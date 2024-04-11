40f84f588005f4b0d9769103fbc8e55fa8a844a4
educavalieri <educavalieriti@gmail.com.br>
2024-04-11 12:23:17 -0300

    iniciando estudos com terraform

---
+++ curso_terraform/aws_bucket/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.44.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "educavalieri"
+      managed-by = "terraform"
+    }
+  }
+}
